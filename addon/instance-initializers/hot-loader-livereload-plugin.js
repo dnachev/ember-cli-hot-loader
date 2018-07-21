9b243206607b0fed923bb90c426a645a2421c32d
Danail Nachev <dnachev@linkedin.com>
2018-07-21 14:58:38 -0700

    Refactor the live plugin to use ES6 syntax class

--- addon/instance-initializers/hot-loader-livereload-plugin.js
+++ addon/instance-initializers/hot-loader-livereload-plugin.js
@@ -1,6 +1,9 @@
 import {all, Promise as rsvpPromise} from 'rsvp';
 
-const DONT_RELOAD_PATTERNS = [/^(?!\/?assets\/).*/, /^\/?assets\/vendor-static\.js/];
+const DONT_RELOAD_PATTERNS = [
+  /^(?!\/?assets\/).*/,
+  /^\/?assets\/vendor-static\.js/,
+];
 
 function shouldReloadScript(scriptEl) {
   if (!scriptEl || scriptEl.getAttribute('src') == null) {
@@ -10,83 +13,78 @@ function shouldReloadScript(scriptEl) {
   return !DONT_RELOAD_PATTERNS.some(pattern => pattern.test(src));
 }
 
-function loadScript(src) {
-  return new rsvpPromise((resolve, reject) => {
-    const scriptEl = document.createElement('script');
-    scriptEl.onload = function() {
-      resolve();
-    };
-    scriptEl.onerror = function(e) {
-      reject(e);
-    };
-    scriptEl.type = 'text/javascript';
-    scriptEl.src = src;
-    document.body.appendChild(scriptEl);
-  });
-}
 
-function createPlugin(hotReloadService) {
-  function Plugin(window, host) {
-    this.window = window;
-    this.host = host;
-  }
-  Plugin.identifier = 'ember-hot-reload';
-  Plugin.version = '1.0'; // Just following the example, this might not be even used
-  Plugin.prototype.reload = function(path) {
-    const cancelableEvent = {modulePath: path, cancel: false};
-    hotReloadService.trigger('willLiveReload', cancelableEvent);
-    if (cancelableEvent.cancel) {
-      // Only hotreload if someone canceled the regular reload
-      // Reloading app.js will fire Application.create unless we set this.
-      window.runningTests = true;
-
-      const scriptsToReload = [];
-      const scriptTags = document.getElementsByTagName('script');
-      for (let i = scriptTags.length - 1; i >= 0; i--) {
-        if (shouldReloadScript(scriptTags[i])) {
-          scriptsToReload.push(scriptTags[i]);
-          scriptTags[i].parentNode.removeChild(scriptTags[i]);
-        }
-      }
-
-      const pendingScripts = scriptsToReload.map(oldScriptEl =>
-        loadScript(oldScriptEl.getAttribute('src')),
-      );
-
-      all(pendingScripts).then(() => {
-        setTimeout(function() {
-          window.runningTests = false;
-          hotReloadService.trigger('willHotReload', path);
-        }, 10);
-      });
-
-      return true;
+function definePlugin(hotReloadService) {
+  const LivereloadPlugin = class {
+    constructor(_window) {
+      this.window = _window;
     }
-    return false;
-  };
-  Plugin.prototype.analyze = function() {
-    return {
-      disable: false,
-    };
+
+    reload(path) {
+      const cancelableEvent = {modulePath: path, cancel: false};
+      hotReloadService.trigger('willLiveReload', cancelableEvent);
+      if (cancelableEvent.cancel) {
+        // Only hotreload if someone canceled the regular reload
+        // Reloading app.js will fire Application.create unless we set this.
+        this.window.runningTests = true;
+
+        const scriptsToReload = [];
+        const scriptTags = this.window.document.getElementsByTagName('script');
+        for (let i = scriptTags.length - 1; i >= 0; i--) {
+          if (shouldReloadScript(scriptTags[i])) {
+            scriptsToReload.push(scriptTags[i]);
+            scriptTags[i].parentNode.removeChild(scriptTags[i]);
+          }
+        }
+
+        const pendingScripts = scriptsToReload.map(oldScriptEl =>
+          this._loadScript(oldScriptEl.getAttribute('src')),
+        );
+
+        all(pendingScripts).then(() => {
+          setTimeout(function() {
+            this.window.runningTests = false;
+            hotReloadService.trigger('willHotReload', path);
+          }, 10);
+        });
+
+        return true;
+      }
+      return false;
+    }
+
+    analyze() {
+      return {
+        disable: false,
+      };
+    }
+
+    _loadScript(src) {
+      return new rsvpPromise((resolve, reject) => {
+        const scriptEl = this.window.document.createElement('script');
+        scriptEl.onload = function() {
+          resolve();
+        };
+        scriptEl.onerror = function(e) {
+          reject(e);
+        };
+        scriptEl.type = 'text/javascript';
+        scriptEl.src = src;
+        document.body.appendChild(scriptEl);
+      });
+    }
   };
 
-  return Plugin;
-}
-
-function lookup(appInstance, fullName) {
-  if (appInstance.lookup) {
-    return appInstance.lookup(fullName);
-  }
-  return appInstance.application.__container__.lookup(fullName);
+  LivereloadPlugin.identifier = 'ember-hot-reload';
+  LivereloadPlugin.version = '1.0';
+  return LivereloadPlugin;
 }
 
 export function initialize(appInstance) {
   if (!window.LiveReload) {
     return;
   }
-  const Plugin = createPlugin(
-    lookup(appInstance, 'service:hot-reload'),
-  );
+  const Plugin = definePlugin(appInstance.lookup('service:hot-reload'));
   window.LiveReload.addPlugin(Plugin);
 }
 
